f735cce7b7180ba42b715d2120bee1a4be0cae22
Amadeusz <amadi9205@gmail.com>
2024-03-26 22:27:40 +0100

    add structure js

--- app.js
+++ app.js
@@ -5,4 +5,21 @@ const valueHex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F
 
 const getHexRandom = () =>{
     const randomIndex = Math.floor(Math.random() * valueHex.length)
+    return randomIndex
 }
+
+const getValueHexString = (lenght) =>{
+    let hexString = '';
+    for(let i = 0; i < lenght; i++){
+        hexString += getHexRandom();
+    }
+    return hexString;
+}
+
+newColor.addEventListener('click', () => {
+    const randomString = '#' +  getValueHexString(6);
+    document.body.style.setProperty({
+        'background-color',
+
+    })
+});
