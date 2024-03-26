d0b932ac052ef86407f0a98cddb6aa4e74d9cd69
Amadeusz <amadi9205@gmail.com>
2024-03-26 22:11:23 +0100

    add structure js

---
+++ app.js
@@ -0,0 +1,8 @@
+const newColor = document.querySelector('#generator-color-button');
+const currentColor = document.querySelector('#display-color');
+
+const valueHex = ['0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F']
+
+const getHexRandom = () =>{
+    const randomIndex = Math.floor(Math.random() * valueHex.length)
+}
